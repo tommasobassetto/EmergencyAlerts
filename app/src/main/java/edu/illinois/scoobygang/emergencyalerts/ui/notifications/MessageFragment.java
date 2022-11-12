03411bcc000cd1c3649d0b0ab4c28f877ef253a8
waihins <weixuansun1@gmail.com>
2022-11-12 17:55:48 -0600

    implemented simple popup window

--- app/src/main/java/edu/illinois/scoobygang/emergencyalerts/ui/notifications/MessageFragment.java
+++ app/src/main/java/edu/illinois/scoobygang/emergencyalerts/ui/notifications/MessageFragment.java
@@ -1,13 +1,22 @@
 package edu.illinois.scoobygang.emergencyalerts.ui.notifications;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -47,6 +56,7 @@ public class MessageFragment extends Fragment {
         listener = new ClickListener() {
             @Override
             public void click(int index){
+                onButtonShowPopupWindowClick(root, inflater);
                 Toast.makeText(root.getContext(),"clicked item index is "+index,Toast.LENGTH_LONG).show();
             }
         };
@@ -73,6 +83,30 @@ public class MessageFragment extends Fragment {
         list.add(new Message("REALLY I DO", "REALLY I DO"));
 
         return list;
+    }
+
+    public void onButtonShowPopupWindowClick(View view, LayoutInflater inflater) {
+        // inflate the layout of the popup window
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
     @Override
