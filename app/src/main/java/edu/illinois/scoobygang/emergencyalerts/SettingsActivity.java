34bd7a926fcd80149415594d05129b55261ef234
aleenareh <air.aleena.rehman@gmail.com>
2022-11-16 00:07:49 -0600

    Finish import contact workflow

--- app/src/main/java/edu/illinois/scoobygang/emergencyalerts/SettingsActivity.java
+++ app/src/main/java/edu/illinois/scoobygang/emergencyalerts/SettingsActivity.java
@@ -1,25 +1,39 @@
 package edu.illinois.scoobygang.emergencyalerts;
 
+import static android.content.ContentValues.TAG;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.core.app.ActivityCompat;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.activity_settings);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},1);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
@@ -43,9 +57,98 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+        @SuppressLint("Range")
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            Preference phoneContacts = findPreference("phone_contacts");
+
+            assert phoneContacts != null;
+            phoneContacts.setOnPreferenceClickListener(preference -> {
+                importPhoneContacts();
+                // popup flow.
+                LayoutInflater inflater = (LayoutInflater) getContext()
+                        .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_import_contact_success, null);
+                ShowSuccessPopUp(inflater, popupView, getView());
+
+                return true;
+            });
+
+        }
+
+        @SuppressLint("Range")
+        public void importPhoneContacts() {
+            ContentResolver cr = getActivity().getContentResolver();
+            Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                    null, null, null, null);
+
+            if ((cur != null ? cur.getCount() : 0) > 0) {
+                while (cur.moveToNext()) {
+                    @SuppressLint("Range") String id = cur.getString(
+                            cur.getColumnIndex(ContactsContract.Contacts._ID));
+                    @SuppressLint("Range") String name = cur.getString(cur.getColumnIndex(
+                            ContactsContract.Contacts.DISPLAY_NAME));
+
+                    if (cur.getInt(cur.getColumnIndex(
+                            ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                        Cursor pCur = cr.query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                        while (pCur.moveToNext()) {
+                            String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            SharedPreferences contactIdPrefs = getActivity()
+                                    .getSharedPreferences("contactID", MODE_PRIVATE);
+                            SharedPreferences.Editor contactIDEditor = contactIdPrefs.edit();
+
+                            int contactID = contactIdPrefs.getInt("count", -1) + 1;
+                            contactIDEditor.putInt("count", contactID);
+                            contactIDEditor.apply();
+                            SharedPreferences contactPrefs = getActivity().
+                                    getSharedPreferences(Integer.toString(contactID), MODE_PRIVATE);
+                            SharedPreferences.Editor contactEditor = contactPrefs.edit();
+
+                            contactEditor.putString("contactID", Integer.toString(contactID));
+                            contactEditor.putString("name", name);
+                            contactEditor.putString("phone", phoneNo);
+                            contactEditor.putString("email", "");
+                            contactEditor.apply();
+
+                            // Todo: add phone platform
+                            Log.i(TAG, "Name: " + name);
+                            Log.i(TAG, "Phone Number: " + phoneNo);
+                        }
+                        pCur.close();
+                    }
+                }
+            }
+            if(cur!=null){
+                cur.close();
+            }
+        }
+
+        public void ShowSuccessPopUp(LayoutInflater inflater, View popupView, View view) {
+            // create the popup window
+            int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            boolean focusable = true; // lets taps outside the popup also dismiss it
+            final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+            // show the popup window
+            // which view you pass in doesn't matter, it is only used for the window
+            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+            // dismiss the popup window when touched
+            popupView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    popupWindow.dismiss();
+                    return true;
+                }
+            });
         }
     }
+
 }
